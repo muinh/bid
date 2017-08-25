2347ca0881720c8e12373ec67992df136827dccf
muinh <dmytropopov.ua@gmail.com>
2017-08-25 22:01:40 +0300

    Realized featured events and category pages

---
+++ controllers/AppController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muinh
+ * Date: 8/24/17
+ * Time: 1:06 PM
+ */
---
+++ views/category/view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muinh
+ * Date: 8/25/17
+ * Time: 2:43 PM
+ */
